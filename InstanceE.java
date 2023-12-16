4869c725a9bceb66179abbaa3360edc1c9859d1c
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-16 14:45:04 +0530

    Instance Block

---
+++ InstanceE.java
@@ -0,0 +1,14 @@
+public class InstanceE {
+
+    int a=10; static int b=20;
+
+    {
+        System.out.println(a+" "+b);
+    }
+
+    public static void main(String []args)
+    {
+        InstanceE Obj=new InstanceE();
+    }
+    
+}
