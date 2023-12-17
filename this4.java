f01cf9bf0b8d7be2c1ceccb046b869d264991c04
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-17 22:22:52 +0530

    this->Call Parametrized Constructor

---
+++ this4.java
@@ -0,0 +1,20 @@
+class this4
+{
+    this4()
+    {
+        this(10);
+
+    }
+
+    this4(int a)
+    {
+        System.out.println(a);
+    }
+
+    public static void main(String []args)
+    {
+         this4 Obj=new this4();
+    }
+
+}
+//(4)-> It is also called Parameterized Constructor of its own class
